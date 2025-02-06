fb69139a6bf36903b35253911106dd465bf5e907
boradesushant <boradesushant50@gmail.com>
2025-02-06 16:57:02 +0530

    Teast_Com_a13

---
+++ src/test/java/Com_Test_2/Test_Com_a13.java
@@ -0,0 +1,20 @@
+package Com_Test_2;
+
+import org.testng.annotations.Test;
+
+public class Test_Com_a13 {
+
+	@Test 
+	
+	public void TC_001 () {
+		
+		
+		System.out.println("Test TC_001");
+		
+	}
+	
+	
+	
+	
+	
+}
